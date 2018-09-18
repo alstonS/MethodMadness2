98dda5ee35c6708e3b6084596beba78ce0bdb533
AlstonS <ashi3454@bths.edu>
2018-09-18 10:13:37 -0400

    End of class.

--- src/AShi/AShiLib.java
+++ src/AShi/AShiLib.java
@@ -7,4 +7,38 @@ public class AShiLib {
         System.out.println(str);
     }
 
+    public static String dateStr(String str)
+    {
+
+        String updatedDate = "";
+       updatedDate += str.substring(3,5)+ "-" + str.substring(0,2) + "-" + str.substring(6);
+        return(updatedDate);
+
+    }
+
+    public static Boolean isPalindrome(String str2)
+    {
+        String newWord="";
+        for(int i = str2.length()-1; i >=0 ; i--) {
+            newWord += str2.substring(i, i + 1);
+        }
+
+        if(str2.equalsIgnoreCase(newWord))
+        {
+            return (true);
+        }
+        else
+        {
+            return (false);
+        }
+
+
+    }
+
+    public static int sumUpTo(int int1)
+    {
+        
+
+
+    }
 }
--- src/AShi/TestFile.java
+++ src/AShi/TestFile.java
@@ -1,10 +1,19 @@
 package AShi;
+import static AShi.AShiLib.dateStr;
+import static AShi.AShiLib.isPalindrome;
 import static AShi.AShiLib.println;
 public class TestFile {
     public static void main(String[] args)
     {
         println("I love a cup of Java in the morning :0 ");
+
+        String dateStr = dateStr("09/18/2018");
+        println(dateStr);
+
+        System.out.println(isPalindrome("racecar"));
+
     }
+
 }
 
 
